b8935b8656ca22bbff962c679df9ed949964b29d
Andrew Shields <andrewshields@Andrews-iMac.local>
2020-08-14 13:02:16 -0400

    MVP + Stretch

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,17 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.counter = 0
+        self.capacity = capacity
 
     def append(self, item):
-        pass
+        if len(self.storage) < self.capacity:
+            self.storage.append(item)
+        else:
+            self.storage[self.counter] = item
+        self.counter = self.counter + 1
+        if self.counter >= self.capacity:
+            self.counter = 0
 
     def get(self):
-        pass
+        return self.storage
